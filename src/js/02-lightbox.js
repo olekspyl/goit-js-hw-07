fe7f84f9e70dc3455873d69006c1ef47e37cb379
Oleksandra Pylypchuk <lemocream@gmail.com>
2022-07-27 20:13:56 +0200

    attempt

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgs = galleryItems
+    .map(galleryItem => {
+        const itemEl = `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" /></a>` 
+        return itemEl;
+    })
+    .join(" ");
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+listEl.insertAdjacentHTML('afterbegin', imgs);
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener('click', onImageClick);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 250,
+
+})
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image"))
+        return;
+}
+
+
+
+galleryContainer.addEventListener('keydown', onEscPress)
+function onEscPress(event) {
+    if (event.code === 'Escape') {
+        instance.close()
+    }
+}
+    
+
+
+
+
+
